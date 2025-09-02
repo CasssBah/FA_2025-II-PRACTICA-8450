abff495e0ae266c2fa55981ef504ec0f756ea2c1
CasssBah <geraldoarmas1@gmail.com>
2025-09-02 15:31:33 -0500

    subiendo ejercicio-SEM1

---
+++ SEMANA1_C#/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SEMANA1_C_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ejer1();
+            Console.ReadKey();
+        }
+        static void ejer1() 
+        {
+            string nombre, carrera;
+
+            Console.Write("Introduzca su nombre: ");
+            nombre = Console.ReadLine();
+
+            Console.Write("Introduzca su carrera: ");
+            carrera = Console.ReadLine();
+
+            Console.WriteLine($"\n{nombre}, Bienvenido al curso de Fundamentos de Algoritmos de {carrera}.");
+
+            ejer2();
+        }
+        static void ejer2()
+        {
+            string nombre;
+            Console.Write("\nIntroduzca su nombre: ");
+            nombre = Console.ReadLine();
+
+            Console.WriteLine("Tu nombre es:" + '"' + nombre + '"' );
+        }
+        static void ejer3() 
+        {
+
+        }
+        static void ejer4()
+        {
+
+        }
+        static void ejer5() 
+        { 
+        
+        }
+        static void ejer6() 
+        { 
+        
+        }
+        static void ejer7() 
+        { 
+        
+        }
+        static void ejer8() 
+        { 
+        
+        }
+        static void ejer9() 
+        { 
+        
+        }
+        static void ejer10() 
+        { 
+        
+        }
+
+    }
+}
---
+++ SEMANA1_PYTHON/SEMANA1_PYTHON.py
@@ -0,0 +1,7 @@
+def ejer1():
+    nombre = input("Introduzca su nombreP: ")
+    carrera = input("Ingrese su carrera: ")
+
+    print (f"\n{nombre}, Bienvenido al curso de Fundamentos de Algoritmos de {carrera}")
+
+ejer1()
