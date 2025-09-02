669fd0cb470ddfeaa73a3a8d33f86c1d641a9acc
Pablo <pablojoelcastilloflores@gmail.com>
2025-09-02 15:31:18 -0500

    Subiendo ejercicio ejercicio1-SEM1

---
+++ SEMANA1_C#/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SEMANA1_C_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ejer1();
+            Console.ReadKey();
+        }
+
+        static void ejer1()
+        {
+            string nombre, carrera; //declarando variables
+            Console.Write("Ingrese su nombre: ");
+            nombre = Console.ReadLine();
+            Console.Write("Ingrese su carrera: ");
+            carrera = Console.ReadLine();
+
+            //Imprimir valores
+            Console.WriteLine($"\n{nombre}, bienvenido al curso de {carrera}."); // "\n" genera saltos de vista
+        }
+        static void ejer2()
+        {
+
+        }
+        static void ejer3()
+        {
+
+        }
+        static void ejer4()
+        {
+
+        }
+        static void ejer5()
+        {
+
+        }
+        static void ejer6()
+        {
+
+        }
+        static void ejer7()
+        {
+
+        }
+        static void ejer8()
+        {
+
+        }
+        static void ejer9()
+        {
+
+        }
+        static void ejer10()
+        {
+
+        }
+    }
+}
